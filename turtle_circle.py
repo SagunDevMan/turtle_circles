e05d89c1d0b4de1dd81bba68fadadc73d3b32fcb
Sagun <125622397+SagunDevMan@users.noreply.github.com>
2023-03-02 02:21:16 -0600

    Improved the comments and documentations

--- turtle_circle.py
+++ turtle_circle.py
@@ -1,31 +1,44 @@
-#program: Make a circle outside of another circle
-#author: SD
-#date: 2/28/2023
-
+# -------------------------------------------
+# Program: Make a circle outside of another circle
+# Author: SD
+# Date: 2/28/2023
+# -------------------------------------------
 
 import turtle
 
-#sets the starting y coord
+# Starting_number defines the starting y_coord, and how much it should go down by
 starting_number = 50
-#starting loop increment 
+# x is used for the while loop, so it should stay zero
 x = 0
+
+
+# Starting the turtle
 turtle.showturtle()
-starting_number1 = starting_number
 turtle.penup()
-#if x is 10 or greater, then stop the program
-while x < 10: # 10 can be changed to get more circles
-    #turns the turtle 180 degrees, so the circles are all facing the same direction
+starting_number1 = starting_number
+
+# Increase the literal number to increase how long the program should run for.
+# This also increase the amount of circles
+while x < 20: 
+
+    # Turns the turtle 180 degrees to the right so the all circles are the same direction
     turtle.right(180)
-    #if x is not 0, then make the circle, if it is, then don't
+
+    # If x is not 0, then make the circle, if not don't
+    # This resolves the bug, where the turtle would draw a circle at 0, 0 at the start of the program
     if not x == 0:
         turtle.circle(starting_number1)
-    #subtract the starting number from the initial number, so y can change
+
+    # Subtracts the starting_number1 by starting_number (default: 50) and assigns the difference to starting_number1
+    # Ex: 50 - 50 = 0, 0 - 50 = -50, -50 - 50 = -100, and so on
     starting_number1 = starting_number1 - starting_number
     turtle.penup()
     turtle.goto(0, starting_number1)
     turtle.pendown()
     turtle.right(180)
-    #print the y coord
+    
+    # Prints the y-coord 
     print("y =", starting_number1)
+
     x += 1
     
